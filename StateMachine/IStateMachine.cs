a60c9898a847f472459c6e7b8b23cfac73b5c124
litaff <a.gazda2k18@gmail.com>
2024-07-08 17:57:20 +0200

    Documentation

--- StateMachine/IStateMachine.cs
+++ StateMachine/IStateMachine.cs
@@ -6,7 +6,19 @@ public interface IStateMachine<T> : IDisposable where T : Enum
     public State<T>? CurrentState { get; }
     public State<T>? PreviousState { get; }
     
+    /// <summary>
+    /// Should be called to start the state machine.
+    /// </summary>
+    /// <param name="initialStateType">State type which will be entered after setup.</param>
     public void Run(T initialStateType);
+    /// <summary>
+    /// Should be called to switch to a new state.
+    /// </summary>
+    /// <param name="stateType">State type to which the state machine will switch to.</param>
     public void SwitchState(T stateType);
+    /// <summary>
+    /// Should try to switch to the previous state.
+    /// </summary>
+    /// <returns>True if successfully switched the state.</returns>
     public bool TrySwitchToPrevious();
 }
--- StateMachine/State.cs
+++ StateMachine/State.cs
@@ -3,9 +3,22 @@ namespace StateMachine;
 [Serializable]
 public abstract class State<T> where T : Enum
 {
+    /// <summary>
+    /// Contains the state machine that this state belongs to.
+    /// </summary>
     public IStateMachine<T>? StateMachine { get; set; } 
+    /// <summary>
+    /// Returns the type of this state.
+    /// </summary>
     public abstract T StateType { get; }
     
+    /// <summary>
+    /// Called when the state is entered.
+    /// </summary>
     public virtual void OnEnter() { }
+    
+    /// <summary>
+    /// Called when the state is exited.
+    /// </summary>
     public virtual void OnExit() { }
 }
